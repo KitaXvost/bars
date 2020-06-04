acfd56fe8d38a6daedfdaa2b6a4f8ff97dcb59f0
KitaXvost <box1979@mail.ru>
2020-06-04 21:40:18 +0300

    add pop-up(edit) in index.php

--- index.php
+++ index.php
@@ -4,6 +4,23 @@ print'
 <head>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 		<script type="text/javascript" src="../rest/jquery-1.3.2.min.js"></script>
+<script>
+    function close2(){
+    document.getElementById("edit-task").style.visibility = "hidden";
+    }
+    function edit(){
+    	document.getElementById("edit-task").style.visibility = "visible";
+    		$.ajax({
+    			url: "edit.php", // путь к php-обработчику
+    			type: "POST", // метод передачи данных
+    			//data: {id:id, url:url, token:token}, // данные, которые передаем на сервер
+    			success: function(data){
+    							$("#edit-task").html(data);
+    			}
+    		});
+
+    }
+</script>
 </head>';
   $db = pg_connect("host=localhost port=5432 dbname=vue_spa user=andreipunt90 password=Ii2329925");
   $result = pg_query($db,"SELECT * FROM spravka");
@@ -12,7 +29,7 @@ print'
   echo "<td>". $row['family'] ." ". $row['name'] ." ". $row['middle_name'] ."</td>";
   echo "<td>". $row['type_category'] . "</td>";
   echo "</tr>";}echo "</table>
-<p>
+<p style='color:grey;'>
 <span onclick='refresh();'>
 <i class='fa fa-2x fa-refresh'></i>
 </span>
@@ -25,11 +42,27 @@ print'
 <span onclick='trash();'>
 <i class='fa fa-2x fa-trash'></i>
 </span>
-</p>  ";
+</p>
+
+<div id='edit-task'></div>
+";
 
 
 print'
 <style type="text/css">
+#edit-task, #create-task{
+color:white;
+font-size:150%;
+visibility:hidden;
+position: fixed;
+top: 30%;
+left: 10%;
+width:80%;
+height:40%;
+z-index: 99;
+background-color: rgba(38,17,0,0.4);
+padding: 20px;
+}
     table {
         font-family: verdana, arial, sans-serif;
         font-size: 11px;
