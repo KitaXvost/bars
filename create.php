0bbe78cb3c3979747bf8b76339cfbe86349d17fd
KitaXvost <box1979@mail.ru>
2020-06-12 18:32:47 +0300

    some changes

--- create.php
+++ create.php
@@ -1,14 +1,13 @@
 <?php
 include 'config.php';
 
-    $name = $_POST['name'];
-    $family = $_POST['family'];
-    $middle_name = $_POST['middle_name'];
-    $n_reg_doc = $_POST['number'];
-    $power = $_POST['power'];
-    $category = $_POST['category'];
-    $type_category = $_POST['type'];
-
+    $name = $_POST['form_name'];
+    $family = $_POST['form_family'];
+    $middle_name = $_POST['form_middle_name'];
+    $n_reg_doc = $_POST['form_number'];
+    $type_category = $_POST['form_type'];
+    $category = $_POST['form_category'];
+    $power = $_POST['form_power'];
 $query = "INSERT INTO spravka (name, family, middle_name, n_reg_doc, power, category, type_category)
           VALUES (:name, :family, :middle_name, :n_reg_doc, :power, :category, :type_category)";
 $params = [
--- edit.php
+++ edit.php
@@ -1,14 +1,14 @@
 <?php
 include 'config.php';
 
-    $name = $_POST['edit-name'];
-    $family = $_POST['edit-family'];
-    $middle_name = $_POST['edit-middle_name'];
-    $n_reg_doc = $_POST['edit-number'];
-    $power = $_POST['edit-power'];
-    $category = $_POST['edit-category'];
-    $type_category = $_POST['edit-type'];
-    $id = $_POST['edit-id'];;
+    $name = $_POST['form_name'];
+    $family = $_POST['form_family'];
+    $middle_name = $_POST['form_middle_name'];
+    $n_reg_doc = $_POST['form_number'];
+    $type_category = $_POST['form_type'];
+    $category = $_POST['form_category'];
+    $power = $_POST['form_power'];
+    $id = $_POST['form_id'];;
 
 $query = "UPDATE spravka SET name = :name, family = :family, middle_name = :middle_name, n_reg_doc = :n_reg_doc, power = :power, category = :category, type_category = :type_category
              WHERE id = :id";
