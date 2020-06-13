73f1f288f192fa15e932957ccbbc5d19aa9325ae
KitaXvost <box1979@mail.ru>
2020-06-13 20:33:04 +0300

    create OOP-file to delete the client by POST id

---
+++ delete_client.php
@@ -0,0 +1,23 @@
+<?php
+
+if($_POST){
+
+    include_once 'config/database.php';
+    include_once 'objects/client.php';
+
+    $database = new Database();
+    $db = $database->getConnection();
+
+    $client = new Client($db);
+
+    $client->id = $_POST['id'];
+
+    // удаление клиента из БД
+    if($client->delete()){
+        echo "Клиент удален";
+    }
+    else{
+        echo "Невозможно удалить клиента";
+    }
+}
+?>
